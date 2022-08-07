47668dae424fa721fa2937641fc609793c4eaead
es-amit <101783688+es-amit@users.noreply.github.com>
2022-08-07 21:08:19 +0530

    Update week1_assignment1.py

--- week1_assignment1.py
+++ week1_assignment1.py
@@ -1,19 +1,13 @@
 #include <stdio.h>
 int main() {
-    int n,check=0,twopower=1;
-    scanf("%d",&n);
-    while(n>=twopower)
+    int T,N,i,points;
+    scanf("%d",&T);
+    for(i=0;i<T;i++)
     {
-        if(n==twopower)
-        {
-            printf("YES\n");
-            check=1;
-        }
-        twopower*=2;
-    }
-    if(check==0)
-    {
-        printf("NO\n");
+        points=0;
+        scanf("%d",&N);
+        points=(N*(N-1))/2;
+        printf("%d\n",points);
     }
     return 0;
 }
